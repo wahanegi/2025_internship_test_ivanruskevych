5f88963ea3c3d1425356cbf8414db5e6ac854570
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-12-17 11:11:34 +0000

    Add rescue_from to ApplicationController for handling validation, not found, and server errors

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,28 +1,25 @@
 class Api::V1::TweetsController < ApplicationController
-  before_action :authenticate_user!, only: [ :create, :destroy ]
+  before_action :authenticate_user!, only: %i[ create destroy ]
   before_action :set_tweet, only: %i[show destroy]
 
   def index
     @tweets = Tweet.includes(:user).order(created_at: :desc)
-    render json: @tweets.to_json(include: { user: { only: :email } })
+    render json: @tweets.to_json(include: { user: { only: :email } }), status: :ok
   end
 
   def create
     @tweet = current_user.tweets.build(tweet_params)
-    if @tweet.save
-      render json: @tweet, status: :created
-    else
-      render json: @tweet.errors.full_messages, status: :unprocessable_entity
-    end
+    @tweet.save!
+    render json: @tweet, status: :created
   end
 
   def show
-    render json: @tweet
+    render json: @tweet, status: :ok
   end
 
   def destroy
     @tweet&.destroy
-    render json: { message: "Tweet deleted!" }
+    render json: { message: "Tweet deleted!" }, status: :ok
   end
 
   private
